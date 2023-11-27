d3893bcd1525e9aba4d3a2603de8ed2b80a1fc3d
Marvin <140358253+masp2004@users.noreply.github.com>
2023-11-27 11:49:55 +0100

    format

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+R
+"src/main/generator/WilsonsGen.java,1/e/1ed3f95a12bc1f7e63e8be33a54e949c2ed3578d
--- src/main/generator/WilsonsGen.java
+++ src/main/generator/WilsonsGen.java
@@ -1,5 +1,10 @@
 package generator;
 
+import main.Maze;
+import main.MazeGridPanel;
+import util.Cell;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -7,78 +12,73 @@ import java.util.Random;
 import java.util.Stack;
 import java.util.stream.Collectors;
 
-import javax.swing.Timer;
-
-import main.*;
-import util.Cell;
-
 public class WilsonsGen {
 
-	private final List<Cell> grid;
-	private final Stack<Cell> stack = new Stack<Cell>();
-	private Cell current;
-	private final Random r = new Random();
+    private final List<Cell> grid;
+    private final Stack<Cell> stack = new Stack<Cell>();
+    private final Random r = new Random();
+    private Cell current;
 
-	public WilsonsGen(List<Cell> grid, MazeGridPanel panel) {
-		this.grid = grid;
-		current = grid.get(r.nextInt(grid.size()));
-		current.setVisited(true);
-		current = grid.get(r.nextInt(grid.size()));
-		final Timer timer = new Timer(Maze.speed, null);
-		timer.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (!grid.parallelStream().allMatch(c -> c.isVisited())) {
-					carve();
-				} else {
-					current = null;
-					Maze.generated = true;
-					timer.stop();
-				}
-				panel.setCurrent(current);
-				panel.repaint();
-				timer.setDelay(Maze.speed);
-			}
-		});
-		timer.start();
-	}
-	
-	private void carve() {
-		if (current.isVisited()) {
-			addPathToMaze();
-			// TODO: Minor future refinement:
-			/* Do not need to run through whole maze with stream filter.
-			 * Could maintain a list of all cells not in the maze from beginning and remove them 
-			 * from the list as we pop them off the stack in addPathToMaze(). Algorithm should still work as 
-			 * current will never be in maze. When this list is empty we have carved the maze.
-			 */
-			List<Cell> notInMaze = grid.parallelStream().filter(c -> !c.isVisited()).collect(Collectors.toList());
-			if (!notInMaze.isEmpty()) {
-				current = notInMaze.get(r.nextInt(notInMaze.size()));							
-			} else {
-				return;
-			}
-		}
-		current.setPath(true);
-		Cell next = current.getNonPathNeighbour(grid);
-		if (next != null) {
-			stack.push(current);
-			current.removeWalls(next);
-			current = next;
-		} else if (!stack.isEmpty()) {
-			try {
-				current = stack.pop();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	private void addPathToMaze() {
-		grid.parallelStream().filter(c -> c.isPath()).forEach(c -> {
-			c.setVisited(true); 
-			c.setPath(false);
-		});
-		stack.clear();
-	}
+    public WilsonsGen(List<Cell> grid, MazeGridPanel panel) {
+        this.grid = grid;
+        current = grid.get(r.nextInt(grid.size()));
+        current.setVisited(true);
+        current = grid.get(r.nextInt(grid.size()));
+        final Timer timer = new Timer(Maze.speed, null);
+        timer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!grid.parallelStream().allMatch(c -> c.isVisited())) {
+                    carve();
+                } else {
+                    current = null;
+                    Maze.generated = true;
+                    timer.stop();
+                }
+                panel.setCurrent(current);
+                panel.repaint();
+                timer.setDelay(Maze.speed);
+            }
+        });
+        timer.start();
+    }
+
+    private void carve() {
+        if (current.isVisited()) {
+            addPathToMaze();
+            // TODO: Minor future refinement:
+            /* Do not need to run through whole maze with stream filter.
+             * Could maintain a list of all cells not in the maze from beginning and remove them
+             * from the list as we pop them off the stack in addPathToMaze(). Algorithm should still work as
+             * current will never be in maze. When this list is empty we have carved the maze.
+             */
+            List<Cell> notInMaze = grid.parallelStream().filter(c -> !c.isVisited()).collect(Collectors.toList());
+            if (!notInMaze.isEmpty()) {
+                current = notInMaze.get(r.nextInt(notInMaze.size()));
+            } else {
+                return;
+            }
+        }
+        current.setPath(true);
+        Cell next = current.getNonPathNeighbour(grid);
+        if (next != null) {
+            stack.push(current);
+            current.removeWalls(next);
+            current = next;
+        } else if (!stack.isEmpty()) {
+            try {
+                current = stack.pop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void addPathToMaze() {
+        grid.parallelStream().filter(c -> c.isPath()).forEach(c -> {
+            c.setVisited(true);
+            c.setPath(false);
+        });
+        stack.clear();
+    }
 }
