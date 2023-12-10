69899349cafaed60608d57c2d90c7f541b12ba22
Marvin <140358253+masp2004@users.noreply.github.com>
2023-12-10 21:22:42 +0100

    disjointsetstest

---
+++ src/test/java/DisjointSetsTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+public class DisjointSetsTest {
+
+    @org.junit.Test
+    public void create_set() {
+
+        util.DisjointSets disjointSets = new util.DisjointSets();
+        int element = 42;
+
+        disjointSets.create_set(element);
+
+        int representativeKey = disjointSets.find_set(element);
+
+        assertEquals(element, representativeKey);
+    }
+
+    public void union() {
+
+        util.DisjointSets disjointSets = new util.DisjointSets();
+        int first = 42;
+        int second = 43;
+
+        disjointSets.create_set(first);
+        disjointSets.create_set(second);
+
+        disjointSets.union(first, second);
+
+        int firstRepresentativeKey = disjointSets.find_set(first);
+        int secondRepresentativeKey = disjointSets.find_set(second);
+
+        assertEquals(firstRepresentativeKey, secondRepresentativeKey);
+    }
+}
